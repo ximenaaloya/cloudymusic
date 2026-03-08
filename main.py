88a10472180ef7420cfd04a4735ad7e9d83a6940
FlorOchoa29 <oflor8718@gmail.com>
2026-03-07 22:37:03 -0600

    Avance

--- main.py
+++ main.py
@@ -16,16 +16,31 @@ class Profile(QtWidgets.QMainWindow):
         super().__init__()
         uic.loadUi("./views/cloudy_profile.ui", self)
 
+class Library(QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.load_ui("./views/cloudy_library.ui,self")
+
 class AppManager:
     def __init__(self):
         self.login_window = Login()
-        self.profile_window = Profile()
+        self.cloudy_profile_window = Profile()
         self.login_window.login_successful.connect(self.show_main_window)
         self.login_window.show()
     def show_main_window(self):
-        self.profile_window.show()
+        self.cloudy_profile_window.show()
         self.login_window.close()
-        
+
+class AppManager:
+    def __init__(self):
+        self.cloudy_profile_window = Profile()
+        self.library_window = Library()
+        self.cloudy_profile_window.profile_successful.connect(self.show_main_window)
+        self.cloudy_profile_window.show()
+    def show_main_window(self):
+        self.library_window.show()
+        self.cloudy_profile_window.close()
+
 app = QtWidgets.QApplication(sys.argv)
 manager = AppManager()
 sys.exit(app.exec())
