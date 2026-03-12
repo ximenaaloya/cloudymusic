a95e1ea2dc406d6851054ef4acc1bc3d5ab17fad
FlorOchoa29 <oflor8718@gmail.com>
2026-03-12 14:54:21 -0600

    Avance

--- controllers/playlist_controller.py
+++ controllers/playlist_controller.py
@@ -4,4 +4,7 @@ class PlaylistController:
     def __init__(self, window, model):
         self.window = window
         self.model = model
-      
+        self.window.btn_agregar.clicked.connect(self.handle_agregar)
+
+    def handle_agregar(self):
+        self.window.playlist_successful.emit()
